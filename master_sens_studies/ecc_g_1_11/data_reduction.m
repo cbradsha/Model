fad21fa8bca0ce832352743e654c3c5d846e5215
Craig <cbradsha@purdue.edu>
2011-01-05 16:54:47 -0500

    asdf
    Signed-off-by: Craig <cbradsha@purdue.edu>

--- master_sens_studies/ecc_g_1_11/data_reduction.m
+++ master_sens_studies/ecc_g_1_11/data_reduction.m
@@ -12,7 +12,7 @@ combined_file = 'data_log_ecc_g.xlsx';
 [num,txt]=xlsread(log_file);
 [num2,txt2]=xlsread('header.xlsx');
 
-if length(txt) == 0
+if exist(combined_file,'file') == 0
     
     xlswrite(combined_file,txt2)
     xlswrite(combined_file,num,'sheet1','A3')
@@ -23,119 +23,58 @@ end
 close all
 
 colors=char('b','k','r','bo-','ko-','ro-');
-g=18;    %Sweeping leakage gap
+g=[18,15];    %Sweeping leakage gap (18), and mu (15)
 
-figure
+for z=1:1:length(g)
 
-% Power input vs. leakage gap
-P_electric=12; %y is P_electric
-n=1;        %initial starting point.
-subplot(3,3,1)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,P_electric),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
+    %Figure 1
+    figure
+    var=[12 15 6 19 20 7 45 49 51];
+    % 12 - P_electric
+    % 15 - mu
+    % 6  - m_dot
+    % 19 - eta_o
+    % 20 - eta_vol
+    % 7  - m_dot_leakage
+    % 45 - W_dot_total
+    % 49 - W_dot_leakage_loss
+    % 51 - Equilibrium Position
+
+    for i=1:1:length(var)
+        vars=var(i); %y is m_dot
+        n=1;        %initial starting point.
+        subplot(3,3,i)
+        for j=1:1:6
+            plot(num(n:n+5,g(z)),num(n:n+5,vars),colors(j,:),'LineWidth',2)
+            hold on
+            n=n+6;
+        end
+        xlabel(txt2(1,g(z)))
+        ylabel(txt2(1,vars))
+    end
+
+    %Figure 2
+    figure
+    var=[41 40 21 24 25 44];
+    % 41 - eta_o_test
+    % 40 - eta_is
+    % 21 - T_d
+    % 24 - Q_dot
+    % 25 - Q_dot_cv2
+    % 44 - W_dot_friction
+
+    for i=1:1:length(var)
+        vars=var(i); %y is m_dot
+        n=1;        %initial starting point.
+        subplot(3,3,i)
+        for j=1:1:6
+            plot(num(n:n+5,g(z)),num(n:n+5,vars),colors(j,:),'LineWidth',2)
+            hold on
+            n=n+6;
+        end
+        xlabel(txt2(1,g(z)))
+        ylabel(txt2(1,vars))
+    end
+
 end
-xlabel(txt2(1,g))
-ylabel(txt2(1,P_electric))
 
-
-% Friction factor vs. leakage gap
-mu=15; %y is mu
-n=1;        %initial starting point.
-subplot(3,3,2)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,mu),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,mu))
-
-
-% Mass Flow vs. leakage gap
-m_dot=6; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,3)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,m_dot),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,m_dot))
-
-
-% Leakage Mass Flow vs. leakage gap
-m_dot_leak=7; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,6)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,m_dot_leak),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,m_dot_leak))
-
-% eta_o vs. leakage gap
-eta_o=19; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,4)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,eta_o),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,eta_o))
-
-% eta_vol vs. leakage gap
-eta_vol=20; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,5)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,eta_vol),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,eta_vol))
-
-
-% W_dot_total vs. leakage gap
-W_dot_total=45; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,7)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,W_dot_total),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,W_dot_total))
-
-% W_dot_leakage vs. leakage gap
-W_dot_leakage=49; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,8)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,W_dot_leakage),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,W_dot_leakage))
-
-
-% Equilibrium Position vs. leakage gap
-eq=51; %y is m_dot
-n=1;        %initial starting point.
-subplot(3,3,9)
-for j=1:1:6
-    plot(num(n:n+5,g),num(n:n+5,eq),colors(j,:),'LineWidth',2)
-    hold on
-    n=n+6;
-end
-xlabel(txt2(1,g))
-ylabel(txt2(1,eq))
