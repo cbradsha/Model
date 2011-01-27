a613880e56cd0021e5b11f9e11668f8f53da0385
Craig <cbradsha@purdue.edu>
2011-01-27 14:37:27 -0500

    upgraded Brent's method, added add-on's to tarball
    Signed-off-by: Craig <cbradsha@purdue.edu>

--- brents.m
+++ brents.m
@@ -1,6 +1,8 @@
 function [p, error_stroke,a,below,above] = brents(p,a,error_brents)
 %function help here
 
+tol = 1e-5;
+
 if p.brent == 1
     p.x_stroke_brent(a) = p.x_stroke;
     if a >= 2   %a greater than 1
@@ -11,31 +13,158 @@ if p.brent == 1
             p.above = a;
         end            
         
+        %both previous pts are below desired stroke, secant only
         if p.x_stroke_brent(a) < p.x_stroke_ref && p.x_stroke_brent(a-1) < p.x_stroke_ref
             %Secant Step
             p.P_brent(a+1) = p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
+            p.method(a)=1;  %1 is secant, 2 is bisection
             
+        %One below, one above (straddled), employ brents method    
         elseif p.x_stroke_brent(a) < p.x_stroke_ref || p.x_stroke_brent(a-1) < p.x_stroke_ref
-            %Bi-section Step
-            p.P_brent(a+1) = (p.P_brent(a)+p.P_brent(a-1))/2;
-            
-        elseif p.x_stroke_brent(a) > p.x_stroke_ref && p.x_stroke_brent(a-1) > p.x_stroke_ref
-            if abs(p.x_stroke_brent(a) - p.x_stroke_brent(a-1)) > 500*error_brents
-                %Secant Step
-                p.P_brent(a+1) = p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));  
-                
-            else
-                %Bi-section Step using 'below'
-                if p.below == 0
-                    p.P_brent(a+1) = 0.5*p.P_brent(1);
+            %If there is only one point below, then...
+            if a==2
+                %Bi-section Step
+                p.P_brent(a+1) = (p.P_brent(a)+p.P_brent(a-1))/2;
+                p.method(a)=2;
+            else   %Brents method start
+                %if the current iterate is above the reference
+                if p.P_brent(a) >= p.x_stroke_ref
+                    b_k = p.P_brent(a);
+                    a_k = p.P_brent(p.below);
+                    b_k_1 = p.P_brent(a-1);
+                    %bisection step
+                    m=(p.P_brent(a)+p.P_brent(a-1))/2;
+                    %Secant step
+                    s=p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
+
+                    if s < b_k && s > m
+                        p.P_brent(a+1)=s;
+                        p.method(a)=1;
+                    else
+                        p.P_brent(a+1)=m;
+                        p.method(a)=2;
+                    end
+                %if the current iterate is below the reference
                 else
-                    p.P_brent(a+1) = (p.P_brent(a)+p.P_brent(p.below))/2;
+                    b_k = p.P_brent(a);
+                    a_k = p.P_brent(p.above);
+                    b_k_1 = p.P_brent(a-1);
+                    %bisection step
+                    m=(p.P_brent(a)+p.P_brent(a-1))/2;
+                    %Secant step
+                    s=p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
+
+                    if s < b_k && s > m
+                        p.P_brent(a+1)=s;
+                        p.method(a)=1;
+                    else
+                        p.P_brent(a+1)=m;
+                        p.method(a)=2;
+                    end
+                    
                 end
-            end
+                
+                %Brent's addition to the algorithmn
+                
+                if a>=3
+                    if abs(p.P_brent(a)-p.P_brent(a-1)) < tol
+                        if p.method(a-1) == 1  %used secant method
+                            if abs(p.P_brent(a-1)-p.P_brent(a-2)) < tol
+                                p.P_brent(a+1)=m; 
+                                p.method(a)=2;
+                            end
+                        else
+                            p.P_brent(a+1)=m;
+                            p.method(a)=2;
+                        end
+                    elseif abs(s-b_k) > 0.5*abs(b_k-b_k_1)
+                        if p.method(a-1) == 1  %used secant method
+                            if abs(s-b_k) > 0.5*abs(b_k_1-p.P_brent(a-2))
+                                p.P_brent(a+1)=m; 
+                                p.method(a)=2;
+                            end
+                        else
+                            p.P_brent(a+1)=m;
+                            p.method(a)=2;
+                        end
+                    end
+                end
+                
+            end %brents method end
+
+        
+        %both above the desired stroke, if p.below does exist, employ brents method    
+        elseif p.x_stroke_brent(a) > p.x_stroke_ref && p.x_stroke_brent(a-1) > p.x_stroke_ref
+            if exist(p.below,'var')
+                p.P_brent(a+1)=p.P_brent(a-1)*0.5;
+            else
+                %if the current iterate is above the reference
+                if p.P_brent(a) >= p.x_stroke_ref
+                    b_k = p.P_brent(a);
+                    a_k = p.P_brent(p.below);
+                    b_k_1 = p.P_brent(a-1);
+                    %bisection step
+                    m=(p.P_brent(a)+p.P_brent(a-1))/2;
+                    %Secant step
+                    s=p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
+
+                    if s < b_k && s > m
+                        p.P_brent(a+1)=s;
+                        p.method(a)=1;
+                    else
+                        p.P_brent(a+1)=m;
+                        p.method(a)=2;
+                    end
+                %if the current iterate is below the reference
+                else
+                    b_k = p.P_brent(a);
+                    a_k = p.P_brent(p.above);
+                    b_k_1 = p.P_brent(a-1);
+                    %bisection step
+                    m=(p.P_brent(a)+p.P_brent(a-1))/2;
+                    %Secant step
+                    s=p.P_brent(a) - (p.x_stroke_brent(a) - p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
+
+                    if s < b_k && s > m
+                        p.P_brent(a+1)=s;
+                        p.method(a)=1;
+                    else
+                        p.P_brent(a+1)=m;
+                        p.method(a)=2;
+                    end
+                    
+                end
+                
+                %Brent's addition to the algorithmn
+                
+                if a>=3
+                    if abs(p.P_brent(a)-p.P_brent(a-1)) < tol
+                        if p.method(a-1) == 1  %used secant method
+                            if abs(p.P_brent(a-1)-p.P_brent(a-2)) < tol
+                                p.P_brent(a+1)=m; 
+                                p.method(a)=2;
+                            end
+                        else
+                            p.P_brent(a+1)=m;
+                            p.method(a)=2;
+                        end
+                    elseif abs(s-b_k) > 0.5*abs(b_k-b_k_1)
+                        if p.method(a-1) == 1  %used secant method
+                            if abs(s-b_k) > 0.5*abs(b_k_1-p.P_brent(a-2))
+                                p.P_brent(a+1)=m; 
+                                p.method(a)=2;
+                            end
+                        else
+                            p.P_brent(a+1)=m;
+                            p.method(a)=2;
+                        end
+                    end
+                end
+                
+            end %brents method end
+
         end
         
-        %p.P_brent(a+1) = p.P_brent(a) - (p.x_stroke_brent(a) -
-        %p.x_stroke_ref)*(p.P_brent(a) - p.P_brent(a-1))/(p.x_stroke_brent(a) - p.x_stroke_brent(a-1));
         error_stroke = abs(p.x_stroke_ref - p.x_stroke_brent(a));
         
         a=a+1;
@@ -47,6 +176,7 @@ if p.brent == 1
         a = a+1;
         p.above = 0;
         p.below = 0;
+        p.method=0;
         %keyboard
     end  % end entry logic
     
--- friction_model.m
+++ friction_model.m
@@ -3,20 +3,23 @@ function [ f_friction ] = friction_model( num_inputs )
 %   Detailed explanation goes here
 
 g=num_inputs(12)*1e6; %leakage gap (microns)
+beta = num_inputs(71); %beta factor
 
-%alpha = 3 (g - 0 to 30 microns)
+%beta = 3 (g - 0 to 30 microns)
 %f_friction=0.000979*g^2 - 0.028555*g + 0.377921;     %friction factor for dry friction between teflon piston and aluminum cylinder
 
-%alpha = 5 (g - 0 to 40 microns)
+%beta = 5 (g - 0 to 40 microns)
 %f_friction = 0.000353*g^2 - 0.017133*g + 0.377921;
 
-%alpha = 6 (g - 0 to 50 microns)
+%beta = 6 (g - 0 to 50 microns)
 %f_friction = 0.000245*g^2 - 0.014277*g + 0.377921;
 
-%alpha = 8 (g - 0 to 60 microns)
+%beta = 8 (g - 0 to 60 microns)
 %f_friction = 0.000138*g^2 - 0.010708*g + 0.377921;
 
-%alpha = 12 (g - 0 to 100 microns)
-f_friction = 0.0000612*g^2 - 0.0071386*g + 0.3779207;
+%beta = 12 (g - 0 to 100 microns)
+%f_friction = 0.0000612*g^2 - 0.0071386*g + 0.3779207;
+
+f_friction =(0.0088141/beta)*g^2 - (0.0856637/beta)*g + 0.3779207;
 end
 
